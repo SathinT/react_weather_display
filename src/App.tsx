a10ad1afab2a135cc050cea4735fa331e9e39dc7
Sathin Thenuja <sathinthenuja@gmail.com>
2024-02-21 00:21:40 +0530

    main process done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../src/App.css"
 import  search  from '../src/assests/search button.png'
 import  cloudy  from '../src/assests/clouds.png'
 
-import  maxT  from '../src/assests/Thermometer Up.png'
-import  minT  from '../src/assests/Thermometer Down.png'
-import  Humidity  from '../src/assests/humidity.png'
-import  Wind  from '../src/assests/wind.png'
-import  Pressure  from '../src/assests/Atmospheric Pressure.png'
+// import  maxT  from '../src/assests/Thermometer Up.png'
+// import  minT  from '../src/assests/Thermometer Down.png'
+// import  Humidity  from '../src/assests/humidity.png'
+// import  Wind  from '../src/assests/wind.png'
+// import  Pressure  from '../src/assests/Atmospheric Pressure.png'
 import {SubWeather} from "./view/SubWeather/SubWeather";
+import axios from "axios";
+
+function getDate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${month}/${date}/${year}`;
+}
+
+export interface WeatherData {
+    name:string;
+    main:{
+        temp:number;
+        temp_max:number;
+        temp_min:number;
+        humidity:number;
+        pressure:number;
+    };
+    weather:{
+        0:{
+            main:string;
+            description:string;
+            icon:string;
+        };
+    };
+    wind:{
+        speed:number;
+    };
+    sys: {
+        country: string;
+    };
+}
 
 function App() {
+    const [data,setData] = useState<WeatherData | null>(null)
+    const [location , setLocation] = useState("")
+
+    const [currentDate, setCurrentDate] = useState(getDate());
+
+    const API_KEY = 'c0db5bb41009f0573fcfdb2e3ecd0cf0'
+    const url =`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${location}&appid=${API_KEY}`
+
+
+    const searchBtn = (event:any) => {
+        if (event.key === "Enter") {
+            axios.get(url)
+                .then((response) => {
+                    setData(response.data);
+                    console.log(response.data);
+                });
+        }
+    }
+
+
+
+    const image = "https://openweathermap.org/img/wn/"+data?.weather["0"].icon+"@4x.png"
   return (
    <>
    <div id="main" className="flex-none md:flex">
@@ -25,15 +80,19 @@ function App() {
          md:w-48 md:ml-[110%]
          lg:w-80">
              <input placeholder="Search City..."
-                    className="bg-transparent outline-none w-52 pl-3"></input>
+                    className="bg-transparent outline-none w-52 pl-3"
+                    value={location} onChange={(event) => setLocation(event.target.value)}
+                    onKeyDownCapture={searchBtn}></input>
+             <button>
              <img className="md:-ml-10
              lg:ml-20" src={search}/>
+             </button>
          </div>
 
                <div className="
                text-center pt-5
                lg:text-left lg:pl-72">
-               <h1 className="text-white text-3xl font-bold">January, 2024</h1>
+               <h1 className="text-white text-3xl font-bold">{currentDate}</h1>
                <h1 className="text-white text-5xl font-bold lg:text-4xl">Monday</h1>
                </div>
 
@@ -43,10 +102,10 @@ function App() {
                    <img className="
                    h-40
                    md:h-56 md:w-72
-                   lg:h-40 lg:w-40 lg:ml-16" src={cloudy}/>
+                   lg:h-40 lg:w-40 lg:ml-16" src={image}/>
                    <h1 className="
                    text-4xl text-orange-600 font-bold text-center -mt-5
-                   lg:text-left lg:ml-20">CLOUDY</h1>
+                   lg:text-left lg:ml-24">{data?.weather["0"].main}</h1>
                </div>
 
                <div className="
@@ -55,12 +114,13 @@ function App() {
                    <div className="
                    pt-14 text-center text-8xl font-bold
                    lg:text-left lg:text-9xl">
-                       <h1 className="text-orange-600">23
+                       <h1 className="text-orange-600">{Math.round(data?.main.temp as number)}
                            <span className="text-white">℃</span>
                        </h1>
+
                    </div>
                    <h1 className="text-center text-white text-3xl font-bold
-                   lg:text-left lg:w-40 lg:text-4xl">Matara, Sri Lanka</h1>
+                   lg:text-left lg:text-4xl">{data?.weather["0"].description} ,<br/> {data?.name}</h1>
                </div>
            </div>
        </div>
@@ -74,13 +134,7 @@ function App() {
            flex flex-wrap justify-between mx-3 pt-7
            md:mt-36
            lg:flex-col lg:ml-20 lg:p-5">
-               <SubWeather/>
-              <SubWeather/>
-               <SubWeather/>
-               <SubWeather/>
-               <div className="hidden md:block">
-               <SubWeather/>
-               </div>
+               <SubWeather WeatherData={data}/>
            </div>
 
        </div>
--- src/view/SubWeather/SubWeather.tsx
+++ src/view/SubWeather/SubWeather.tsx
@@ -1,17 +1,69 @@
 import React, {Component} from "react";
 import Wind from "../../assests/wind.png";
+import maxT from "../../assests/Thermometer Up.png";
+import minT from "../../assests/Thermometer Down.png";
+import humidity from "../../assests/humidity.png";
+import pressure from "../../assests/Atmospheric Pressure.png";
+import { WeatherData } from "../../App";
 
-export class SubWeather extends Component {
+
+interface SubWeatherProps {
+    WeatherData: WeatherData | null;
+}
+
+export class SubWeather extends Component<SubWeatherProps> {
     render() {
+        const { WeatherData } = this.props;
+
         return (
             <>
+                <div className="flex p-5">
+                    <img className="h-14 lg:h-16" src={maxT}/>
+                    <div className="pl-3">
+                        <h1 className="text-orange-600 text-2xl font-bold
+                       lg:text-4xl">{WeatherData?.main.temp_max}<span className="text-lg lg:text-xl"> ℃</span></h1>
+                        <h1 className="text-white text-xl
+                       lg:text-2xl">Max Temp</h1>
+                    </div>
+                </div>
+                <div className="flex p-5">
+                    <img className="h-14 lg:h-16" src={minT}/>
+                    <div className="pl-3">
+                        <h1 className="text-orange-600 text-2xl font-bold
+                       lg:text-4xl">{WeatherData?.main.temp_min}<span className="text-lg lg:text-xl"> ℃</span></h1>
+                        <h1 className="text-white text-xl
+                       lg:text-2xl">Min Temp</h1>
+                    </div>
+                </div>
+                <div className="flex md:flex-col lg:flex-col">
+                <div className="flex p-5">
+                    <img className="h-14 lg:h-16" src={humidity}/>
+                    <div className="pl-3">
+                        <h1 className="text-orange-600 text-2xl font-bold
+                       lg:text-4xl">{WeatherData?.main.humidity}<span className="text-lg lg:text-xl">%</span></h1>
+                        <h1 className="text-white text-xl
+                       lg:text-2xl">Humidity</h1>
+                    </div>
+                </div>
                 <div className="flex p-5">
                     <img className="h-14 lg:h-16" src={Wind}/>
                     <div className="pl-3">
                         <h1 className="text-orange-600 text-2xl font-bold
-                       lg:text-4xl">45<span className="text-lg lg:text-xl"> kmp/h</span></h1>
+                       lg:text-4xl">{WeatherData?.wind.speed}<span className="text-lg lg:text-xl"> kmp/h</span></h1>
                         <h1 className="text-white text-xl
                        lg:text-2xl">Wind</h1>
+                    </div>
+                </div>
+                </div>
+                <div className="hidden md:block">
+                    <div className="flex p-5">
+                        <img className="h-14 lg:h-16" src={pressure}/>
+                        <div className="pl-3">
+                            <h1 className="text-orange-600 text-2xl font-bold
+                       lg:text-4xl">{WeatherData?.main.pressure}</h1>
+                            <h1 className="text-white text-xl
+                       lg:text-2xl">Pressure</h1>
+                        </div>
                     </div>
                 </div>
             </>
